d03760decba7c97a9f7ba7576e28f96af3e4e04a
Vladimir <Chernousov2005@mail.ru>
2024-11-02 14:00:00 +0300

    described the connection of the buttons and began to describe the context menu

--- file_manager.py
+++ file_manager.py
@@ -1,7 +1,7 @@
 import sys
 from collections import deque
 from PyQt5.QtCore import QDir
-from PyQt5.QtWidgets import QApplication, QFileSystemModel, QTreeView, QMainWindow
+from PyQt5.QtWidgets import QApplication, QFileSystemModel, QTreeView, QMainWindow, QMenu
 
 import main_window_disigne
 
@@ -18,6 +18,43 @@ class FileManager(QMainWindow):
 
         self.ui.tree.setModel(self.model)
         self.ui.tree.setSelectionMode(QTreeView.ExtendedSelection)
+
+        self.ui.pbn_cut.clicked.connect(self.pbn_cut)
+        self.ui.pbn_copy.clicked.connect(self.pbn_copy)
+        self.ui.pbn_paste.clicked.connect(self.pbn_paste)
+        self.ui.pbn_delete.clicked.connect(self.pbn_delete)
+        self.ui.pbn_info.clicked.connect(self.pbn_info)
+
+        self.ui.actioninfo.triggered.connect(self.menu_info)
+
+
+    def pbn_cut(self):
+        pass
+
+    def pbn_copy(self):
+        pass
+
+    def pbn_paste(self):
+        pass
+
+    def pbn_delete(self):
+        pass
+
+    def pbn_info(self):
+        pass
+
+    def menu_info(self):
+        pass
+
+    '''def contextMenuEvent(self, event):
+        contextMenu = QMenu()
+
+        newAction = contextMenu.addAction("New")
+
+        action = contextMenu.exec_(self.mapToGlobal(event.pos()))
+
+        if action == quit:
+            self.close()'''
 
 
 def main():
