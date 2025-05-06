2ce6f3947dadf638a0938b0a9135386089f0c422
gravenaj <mister.gulin-serg@yandex.ru>
2025-05-06 14:59:58 +0300

    фикс кнопки + форматирование

--- static/script.js
+++ static/script.js
@@ -1,15 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
   const backToTop = document.querySelector(".back-to-top");
 
-  window.addEventListener("scroll", function () {
-    if (window.scrollY > 100) {
-      backToTop.style.display = "block";
-    } else {
-      backToTop.style.display = "none";
-    }
-  });
+  if (backToTop) {
+    window.addEventListener("scroll", function () {
+      if (window.scrollY > 100) {
+        backToTop.style.display = "flex"; // Используем flex вместо block
+      } else {
+        backToTop.style.display = "none";
+      }
+    });
 
-  backToTop.addEventListener("click", function () {
-    window.scrollTo({ top: 0, behavior: "smooth" });
-  });
+    backToTop.addEventListener("click", function (e) {
+      e.preventDefault();
+      window.scrollTo({ top: 0, behavior: "smooth" });
+    });
+  }
 });
